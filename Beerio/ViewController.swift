bc9a83a21a4993af21050c3f96e5883ecba51203
Casper Verswijvelt <casperverswijvelt@gmail.com>
2018-11-21 16:03:58 +0100

    Initial Commit

---
+++ Beerio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beerio
+//
+//  Created by Casper Verswijvelt on 21/11/2018.
+//  Copyright © 2018 Casper Verswijvelt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
